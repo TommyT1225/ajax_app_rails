4e5fe8c5d50e7683b7ff67cc6fa5c75eaea8552b
TommyT1225 <takuyamarumoco@gmail.com>
2023-12-28 15:09:01 +0900

    デプロイの実施

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
-class ApplicationController < ActionController::Base
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.order("id DESC")
+    @article = Article.new
+  end
+
+  def new
+  end
+
+  def create
+    binding.pry
+    article = Article.new(article_params)
+    if article.save
+      render json: {article: article} 
+      redirect_to index 
+    end
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:text)
+  end
 end
+
